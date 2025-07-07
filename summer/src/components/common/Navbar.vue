dd9dd702923f498566cc0eadb58daa1fedea1939
Mikhail <lalkazatralen2@gmail.com>
2025-07-07 13:38:58 +0300

    design small desktop

--- summer/src/components/common/Navbar.vue
+++ summer/src/components/common/Navbar.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-    <div class="flex justify-between mx-5 md:mx-8">
+    <div class="flex justify-between mx-5 md:mx-8 xl:mx-20">
         <router-link to="/">
             <h3 class="text-[28px] md:text-[36px] md:font-medium">Nitro</h3>
         </router-link>
