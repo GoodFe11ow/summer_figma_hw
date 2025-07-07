483321b4a5be26d366abf6c8c8ca9605ac2b1a07
Mikhail <lalkazatralen2@gmail.com>
2025-07-07 11:26:05 +0300

    footer no style

--- summer/src/components/common/Footer.vue
+++ summer/src/components/common/Footer.vue
@@ -2,8 +2,78 @@
     <div>
         <div class="text-6xl font-semibold">
             <h2>Have an idea?</h2>
-            <router-link to="/contact">Let’s Talk</router-link>
+            <router-link to="/contact" class="font-normal underline decoration-3 underline-offset-4">Let’s Talk</router-link>
         </div>
-        <div></div>
+        <div>
+            <div>
+                <div>
+                    <div>
+                        <p>
+                            Get in touch
+                        </p>
+                        <p class="font-bold">
+                            info@template.com
+                        </p>
+                    </div>
+                    <div>
+                        <p class="font-bold">Phone</p>
+                        <p>
+                            +000 111 222 3333<br>+444 555 666 7777
+                        </p>
+                    </div>
+                    <div>
+                        <p class="font-bold">Address</p>
+                        <p>2352 Middlefield Rd, Markham<br>Ontario, Canada</p>
+                    </div>
+                </div>
+                <div>
+                    <p class="font-bold">Pages</p>
+                    <ul>
+                        <li>Home</li>
+                        <li>Service</li>
+                        <li>Case Study</li>
+                        <li>About Us</li>
+                        <li>Blog</li>
+                        <li>contact</li>
+                    </ul>
+                </div>
+                <div>
+                    <p class="font-bold">Admin</p>
+                    <ul>
+                        <li>404</li>
+                        <li>Login</li>
+                        <li>Licenses</li>
+                        <li>Instruction</li>
+                        <li>Styleguide</li>
+                        <li>Changelog</li>
+                        <li>More Templates</li>
+                    </ul>
+                </div>
+                <div>
+                    <div>
+                        <p>Reach Out to Us</p>
+                        <div>
+                            <i class="fa-brands fa-facebook"></i>
+                            <i class="fa-brands fa-twitter"></i>
+                            <i class="fa-brands fa-instagram"></i>
+                            <i class="fa-brands fa-skype"></i>
+                            <i class="fa-brands fa-whatsapp"></i>
+                            <i class="fa-brands fa-linkedin-in"></i>
+                        </div>
+                    </div>
+                    <div>
+                        <p>Join our mailing list and get to know exciting stuff at first hand</p>
+                        <input type="search" placeholder="johnchoe@gmail.com" class="border-1 w-[220px] h-[56px]"><button class="h-[56px] w-[60px] bg-[#00A693]"><i class="fa-regular fa-paper-plane text-white fa-xl"></i></button>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <p>Made it with love by Hexwaave. Powered by Webflow.</p>
+                <div>
+                    <a href="">Policy</a>
+                    <a href="">Sitemap</a>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
