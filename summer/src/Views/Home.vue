e570dc3388e52440b6bccde5ecceee48cb6db682
Mikhail <lalkazatralen2@gmail.com>
2025-08-05 22:06:57 +0300

    our service and card in mobile

--- summer/src/Views/Home.vue
+++ summer/src/Views/Home.vue
@@ -1,7 +1,9 @@
 <template>
     <HomeHero />
+    <OurService />
 </template>
 
 <script setup>
     import HomeHero from '../components/Home/HomeHero.vue';
+    import OurService from '../components/Home/OurService.vue';
 </script>
---
+++ summer/src/components/Home/OurServiceCard.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="bg-[#00211D] mb-8">
+        <div class="pt-8 pl-8">
+            <div class="bg-[#004B49] flex justify-center items-center w-20 h-20 rounded-full">
+                <div class="w-10 h-10"  v-html="svgIcon"></div>
+            </div>
+        </div>
+        <div class="px-8 pt-6 pb-10">
+            <h3 class="text-3xl font-semibold text-white mb-4">{{ role }}</h3>
+            <p class="text-white font-extralight text-[18px]">{{ description }}</p>
+        </div>
+    </div>
+</template>
+
+<script setup>
+    defineProps({
+        svgIcon: String,
+        role: String,
+        description: String
+    })
+</script>
