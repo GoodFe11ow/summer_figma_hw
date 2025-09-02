a35a16aa442257dfc99569bb2278b8e8937d4e9d
Mikhail <lalkazatralen2@gmail.com>
2025-09-02 14:56:57 +0300

    selected works all dimension

--- summer/src/components/Home/WorkCaseCard.vue
+++ summer/src/components/Home/WorkCaseCard.vue
@@ -9,16 +9,18 @@ const props = defineProps({
 </script>
 
 <template>
-  <div class="mx-[6%] border-b-1 border-[#004B49]">
-    <img :src="image" alt="card image" class="h-[210px] w-[280px] object-cover">
-    <div class="flex flex-col gap-5 mt-4 mb-5">
-      <h3 class="text-4xl font-semibold">{{ props.title }}</h3>
-      <div v-if="props.tags && props.tags.length > 0" class="">
-        <span v-for="(tag, index) in props.tags" :key="index" class="bg-[#006458] text-sm px-3 py-1 rounded-full uppercase mr-4">
-          {{ tag }}
-        </span>
+  <a href="#" class="lg:w-[47%] xl:w-[48%]">
+    <div class=" border-b-1 border-[#004B49]">
+      <img :src="image" alt="card image" class="h-[210px] md:h-[438px]  w-[100%] object-cover">
+      <div class="flex flex-col gap-5 mt-4 mb-5 md:flex-row md:justify-between md:items-center">
+        <h3 class="text-4xl font-semibold">{{ props.title }}</h3>
+        <div v-if="props.tags && props.tags.length > 0" class="">
+          <span v-for="(tag, index) in props.tags" :key="index" class="bg-[#006458] text-sm px-3 py-1 rounded-full uppercase mr-4">
+            {{ tag }}
+          </span>
+        </div>
       </div>
+      <p class="text-md pb-4">{{ props.description }}</p>
     </div>
-    <p class="text-lg  pb-4">{{ props.description }}</p>
-  </div>
+  </a>
 </template>
