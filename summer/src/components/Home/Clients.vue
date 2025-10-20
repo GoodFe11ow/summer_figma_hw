3c8009e82a00407387c783b6264a8d5a29ec4701
Mikhail <lalkazatralen2@gmail.com>
2025-10-20 20:43:59 +0300

    add 3 logo and made all size clients

--- summer/src/components/Home/Clients.vue
+++ summer/src/components/Home/Clients.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="bg-[#001412] text-white px-[6%] py-[25%]">
-        <div class="mb-15">
-            <h1 class="text-[36px] text-center">Some of our awesome clients that we are proud to work with</h1>
+    <div class="bg-[#001412] text-white px-[6%] py-[25%] md:px-[4%] md:py-[13%] lg:px-[6%] lg:py-[8%] xl:px-[7%] xl:py-[7%]">
+        <div class="mb-15 md:mb-35">
+            <h1 class="text-[36px] text-center md:text-[48px] md:font-semibold lg:text-[56px]">Some of our awesome clients that we are proud to work with</h1>
         </div>
         <div class="-mx-[6%]">
             <ClientsCarousel />
--- summer/src/components/Home/ClientsCarousel.vue
+++ summer/src/components/Home/ClientsCarousel.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col gap-8">
+  <div class="flex flex-col gap-15 md:gap-25">
       <div class="w-full overflow-hidden group">
         <div class="flex w-max animate-[scroll-left_15s_linear_infinite] group-hover:[animation-play-state:paused]">
           <div v-for="(logo, index) in duplicatedTopRow" :key="index" class="flex-shrink-0 flex items-center justify-center px-8">
@@ -19,23 +19,29 @@
 </template>
 
 <script setup>
+    import logo1 from '../../assets/logos/ferrari.svg';
     import logo2 from '../../assets/logos/Logo2.svg';
     import logo3 from '../../assets/logos/Logo3.svg';
     import logo4 from '../../assets/logos/Logo4.svg';
     import logo5 from '../../assets/logos/Logo5.svg';
     import logo6 from '../../assets/logos/Logo6.svg';
     import logo7 from '../../assets/logos/Logo7.svg';
+    import logo8 from '../../assets/logos/porshe.svg';
+    import logo9 from '../../assets/logos/tesla.svg';
     import logo10 from '../../assets/logos/Logo10.svg';
     
     import { ref, computed } from 'vue';
 
     const logos = ref([
+        {src: logo1, alt: 'Ferrari'},
         { src: logo2, alt: 'Smart'},
         { src: logo3, alt: 'Cadillac'},
         { src: logo4, alt: 'Bentley'},
         { src: logo5, alt: 'Dodge'},
         { src: logo6, alt: 'Kia'},
         { src: logo7, alt: 'Jeep'},
+        { src: logo8, alt: 'Porshe'},
+        { src: logo9, alt: 'Tesla'},
         { src: logo10, alt: 'Ford'},
     ]);
 
