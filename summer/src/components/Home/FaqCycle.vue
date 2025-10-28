1fc4804a0e9e22a69fd5d2dc3e11536c26fcb788
Mikhail <lalkazatralen2@gmail.com>
2025-10-28 07:16:58 +0200

    hover additin class for FAQ buttons

--- summer/src/components/Home/FaqCycle.vue
+++ summer/src/components/Home/FaqCycle.vue
@@ -1,10 +1,10 @@
 <template>
-    <div class="flex gap-5">
+    <div class="flex gap-5 mb-10 justify-center items-center">
         <div class="max-w-[200px]">
-            <p>{{ props.question }}</p>
-            <p v-show="showAnswer">{{ props.answer }}</p>
+            <p class="text-[24px] font-semibold border-b-1 pb-4 border-[#004B49]">{{ props.question }}</p>
+            <p class="pt-5 text-[18px]" v-show="showAnswer">{{ props.answer }}</p>
         </div>
-        <button class="w-15 h-15 bg-[#004B49] rounded-full" @click="showAnswer =! showAnswer">+</button>
+        <button class="w-15 h-15 bg-[#004B49] rounded-full transition-all duration-300" :class="{'rotate-45': showAnswer, 'bg-[#008576]': showAnswer}" @click="showAnswer =! showAnswer"><span class="text-[40px] font-extralight">+</span></button>
     </div>
 </template>
 
